304218600a6af60a8c2ac6bbd34da1579bcdfe6e
abdallah723 <57788908+abdallah723@users.noreply.github.com>
2020-03-19 22:18:27 -0400

    Add files via upload

--- apiRoutes.js
+++ apiRoutes.js
@@ -2,22 +2,39 @@ const fs = require ("fs");
 
 module.exports = function (app) {
     const note = require ("../db/db.json");
-    app.get ("/api/note", (req, res) => {
-        return res.json(note)
+    app.get ("/api/notes", (req, res) => {
+        console.log(note);
+        return res.json(note);
+        
+
     });
 
-    app.post ("/api/note", (req, res) => {
+    app.post ("/api/notes", (req, res) => {
         const newNote = req.body;
-
-        newNote.routeName = req.body.name.split(" ").join("").toLowerCase();
-
+        
+        // create the id here db.json
+        newNote.id = (notes[notes.length-1].id +1);
         note.push(newNote);
+        const jNotes = JSON.stringify(notes);
+        fs.writeFile("./db/db.json", jNotes, function (err) {
+            if (err) throw err;
+        })
+        newNote.routeName = req.body.name.split(" ").join("").toLowerCase();
         res.json(newNote);
     });
 
-    // delete function is WIP
-    app.delete ("/api/note/:", (req, res) => {
-        
+    // delete function is WIP 
+    app.delete ("/api/notes/:id", (req, res) => {
+        const id = req.params.id;
+        notes.forEach((item, index) => {
+            if(id === item.id) {
+                notes.splice(index, 1);
+            }
+            const jNotes = JSON.stringify(notes);
+            fs.writeFile("./db/db.json", jNotes, err => {
+                if (err) throw err;
+                console.log(`Note has been deleted!`)
+            })
+        })
     })
 }
-
